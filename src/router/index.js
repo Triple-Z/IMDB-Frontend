7491c6e86b1076cb2ea0e2431e874d23d96ac459
Triple-R <hyckcs@gmail.com>
2018-07-07 22:33:25 +0800

    Finish router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
-import searchTitles from '../components/searchTitles.vue'
-// import searchNames from '../components/searchNames.vue'
-// import curdNames from '../components/curdNames.vue'
-// import curdTitles from '../components/curdTitles.vue'
-// import curdPrincipals from '../components/curdPrincipals.vue'
-// import advancedSearchNames from '@/components/advancedSearchNames'
-// import advancedSearchTiltes from '@/components/advancedSearchTiltes'
+import searchTitles from '@/components/searchTitles.vue'
+import searchNames from '@/components/searchNames.vue'
+import curdNames from '@/components/curdNames.vue'
+import curdTitles from '@/components/curdTitles.vue'
+import curdPrincipals from '@/components/curdPrincipals.vue'
+import advancedSearchNames from '@/components/advancedSearchNames'
+import advancedSearchTiltes from '@/components/advancedSearchTiltes'
 
 Vue.use(Router)
 
@@ -15,13 +15,38 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'index',
       component: index
     },
     {
       path: '/searchTitles',
       name: 'searchTitles',
       component: searchTitles
+    },
+    {
+      path: '/searchNames',
+      name: 'searchNames',
+      component: searchNames
+    },
+    {
+      path: '/advancedsearchtitles',
+      component: advancedSearchTiltes
+    },
+    {
+      path: '/advancedsearchnames',
+      component: advancedSearchNames
+    },
+    {
+      path: '/curdNames',
+      component: curdNames
+    },
+    {
+      path: '/curdtitles',
+      component: curdTitles
+    },
+    {
+      path: '/curdprincipals',
+      component: curdPrincipals
     }
   ]
 })
